32c30734539a3180f026bb2f545aaff37691acb3
werwerwerx <214026366+werwerwerx@users.noreply.github.com>
2025-11-07 11:20:39 +0500

    base produvt card

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,7 @@ import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 import { Database } from '@/shared/supabase/database.types'
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   let response = NextResponse.next({
     request: {
       headers: request.headers,
